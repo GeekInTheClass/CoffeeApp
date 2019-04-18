2e46e08cec15dcfbdabf19d15d3c5a9f3272b7f5
SungIn <sungin@sunginui-macbook-pro.local>
2019-04-18 16:50:37 +0900

    Initial Commit

---
+++ CoffeeLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoffeeLab
+//
+//  Created by SungIn on 2019. 4. 18..
+//  Copyright © 2019년 SungIn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
